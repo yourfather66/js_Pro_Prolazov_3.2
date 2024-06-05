3bbf4cff55c7dd8e90cd387d8e33639c1ceacb5c
YourFather <igorprolazov@gmail.com>
2024-06-05 22:11:39 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let firstUserNum = prompt(`Enter your first num`);
+let secondUserNum = prompt(`Enter your second num`);
+let thirdUserNum = prompt(`Enter your third num`);
+
+console.log(`${firstUserNum} , ${secondUserNum} , ${thirdUserNum}`);
